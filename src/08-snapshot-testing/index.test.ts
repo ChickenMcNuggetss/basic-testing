efa877b73ae09547e833a615bdc9c782b7d5d1bf
ChickenMcNuggetss <7karina.ibr@gmail.com>
2025-11-10 23:14:39 +0300

    feat: add test 08

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,41 @@
 // Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
 describe('generateLinkedList', () => {
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    const list = ['meow', 'meow2', 'meow3'];
+    const res = generateLinkedList(list);
+    const expectedRes = {
+      value: 'meow',
+      next: {
+        value: 'meow2',
+        next: {
+          value: 'meow3',
+          next: {
+            next: null,
+            value: null,
+          },
+        },
+      },
+    };
+    expect(res).toStrictEqual(expectedRes);
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    const list = ['meow', 'meow2', 'meow3'];
+    const res = generateLinkedList(list);
+    // const expectedRes = {
+    //   value: 'meow',
+    //   next: {
+    //     value: 'meow2',
+    //     next: {
+    //       value: 'meow3',
+    //       next: null,
+    //     },
+    //   },
+    // };
+    expect(res).toMatchSnapshot();
   });
 });
